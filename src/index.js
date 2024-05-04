568f5c6750005d1fd2cccc25570be5b154f8fffd
Matheus Torres <torresmiranda.rj@gmail.com>
2024-05-04 13:23:57 -0300

    feat: start integration between google sheets & hubspot

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
-console.log('Hello World!');
-console.log('Hello, DevApi!');
+import { GoogleAuthorization } from './libs/google-auth.js';
+import { GoogleSheets } from './libs/google-sheets.js';
+import { HubSpot } from './libs/hubspot.js';
+
+const SPREADSHEETS_SCOPES = [
+	'https://www.googleapis.com/auth/spreadsheets',
+	'https://www.googleapis.com/auth/spreadsheets.readonly',
+];
+
+const googleAuthorization = new GoogleAuthorization({
+	clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+	privateKey: process.env.GOOGLE_PRIVATE_KEY,
+	scopes: SPREADSHEETS_SCOPES,
+});
+
+const googleSheets = new GoogleSheets(googleAuthorization);
+const hubSpot = new HubSpot(process.env.HUBSPOT_ACCESS_TOKEN);
+
+(async () => {
+	try {
+		const { spreedsheet } = await googleSheets.getSpreedsheet({
+			spreadsheetId: '1VUP5yPfk25qgDYBB1PrpC-S5hjjGbrKOhmJ_tibeWwA',
+			range: 'Página1!A1:F6',
+		});
+
+		const { contacts: hubSpotContacts } = await hubSpot.createContactsInBatch({
+			inputs: GoogleSheets.mapContactsToHubspot(spreedsheet),
+		});
+
+		console.log(hubSpotContacts.results);
+	} catch (error) {
+		console.error(error);
+	}
+})();
