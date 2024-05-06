ea7e0fc2a27489100d62a76657290ea74b651df6
Matheus Torres <torresmiranda.rj@gmail.com>
2024-05-06 12:18:09 -0300

    test: ensure SUT throws if inputs are not provided

--- tests/libs/hubspot.spec.js
+++ tests/libs/hubspot.spec.js
@@ -1,8 +1,23 @@
-import { throws } from 'node:assert';
+import { rejects, throws } from 'node:assert';
 import { describe, it } from 'node:test';
 
+import { NotificationListener } from '../../src/core/notification/notification-listener.js';
 import { Notification } from '../../src/core/notification/notification.js';
 import { HubSpot } from '../../src/libs/hubspot.js';
+
+function makeSUT(accessToken = process.env.HUBSPOT_ACCESS_TOKEN) {
+	const hubSpotNotification = new Notification();
+	const hubSpotNotificationListener = new NotificationListener(
+		'HubSpotNotificationListener',
+	);
+
+	hubSpotNotification.addListener(hubSpotNotificationListener);
+
+	return {
+		hubSpotNotification,
+		sut: new HubSpot(accessToken, hubSpotNotification),
+	};
+}
 
 describe('HubSpot', () => {
 	it('should throw if required argument accessToken is not provided', () => {
@@ -15,6 +30,16 @@ describe('HubSpot', () => {
 		throws(() => new HubSpot('random_access_token'), {
 			message:
 				'Argument {notification} is required and must be an instance of Notification.',
+		});
+	});
+
+	describe('.createContactsInBatch', () => {
+		it('should throw if an array of inputs are not provided', () => {
+			const { sut } = makeSUT();
+
+			rejects(() => sut.createContactsInBatch({}), {
+				message: 'Argument {inputs} is required and must be an array.',
+			});
 		});
 	});
 });
