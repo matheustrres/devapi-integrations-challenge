93e48c0bb233d85576ef73222586de4c65d784e4
Matheus Torres <torresmiranda.rj@gmail.com>
2024-05-06 11:40:04 -0300

    test: ensure SUT throws if invalid spreedsheet id is provided

--- tests/libs/google-sheets.spec.js
+++ tests/libs/google-sheets.spec.js
@@ -3,10 +3,10 @@ import { describe, it } from 'node:test';
 
 import { GoogleSheets } from '../../src/libs/google-sheets.js';
 
-function makeSUT() {
+function makeSUT(googleApiKey = process.env.GOOGLE_API_KEY) {
 	return {
 		sut: new GoogleSheets({
-			googleApiKey: 'random_google_api_key',
+			googleApiKey,
 		}),
 	};
 }
@@ -19,5 +19,20 @@ describe('GoogleSheets', () => {
 			message:
 				'Both arguments {spreadsheetId} and {range} are required and must be a string.',
 		});
+	});
+
+	it('should throw if an invalid spreedsheet id is provided', async () => {
+		const { sut } = makeSUT();
+
+		rejects(
+			() =>
+				sut.getSpreedsheet({
+					spreadsheetId: 'invalid_spreedsheet_id',
+					range: 'Página1!A1:E20',
+				}),
+			{
+				message: 'Requested entity was not found.',
+			},
+		);
 	});
 });
